7f15191732a7a04db5f232e35664db56adaf05cd
FaryaN <38796279+faryanra@users.noreply.github.com>
2026-02-13 10:43:28 +0100

    feat: complete Main DI container with all service registrations

--- src/main.php
+++ src/main.php
@@ -11,6 +11,13 @@
 
 namespace Notification_Hub;
 
+use Notification_Hub\Repositories\Notifications;
+use Notification_Hub\Repositories\Custom_Hooks;
+use Notification_Hub\Services\Notification_Dispatcher;
+use Notification_Hub\Presenters\Admin\Dashboard_Page;
+use Notification_Hub\Presenters\Admin\Hooks_Page;
+use Notification_Hub\Presenters\Admin\Settings_Page;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -48,10 +55,81 @@ class Main {
 	}
 
 	/**
-	 * Private constructor to prevent direct instantiation.
+	 * Constructor - register all services.
 	 */
-	private function __construct() {
-		// Services will be registered by Loader.
+	public function __construct() {
+		$this->register_repositories();
+		$this->register_services();
+		$this->register_presenters();
+	}
+
+	/**
+	 * Register repositories.
+	 *
+	 * @return void
+	 */
+	private function register_repositories() {
+		// Notifications Repository.
+		$this->set(
+			'notifications_repo',
+			function () {
+				return new Notifications();
+			}
+		);
+
+		// Custom Hooks Repository.
+		$this->set(
+			'custom_hooks_repo',
+			function () {
+				return new Custom_Hooks();
+			}
+		);
+	}
+
+	/**
+	 * Register services.
+	 *
+	 * @return void
+	 */
+	private function register_services() {
+		// Notification Dispatcher.
+		$this->set(
+			'notification_dispatcher',
+			function () {
+				return new Notification_Dispatcher();
+			}
+		);
+	}
+
+	/**
+	 * Register presenters.
+	 *
+	 * @return void
+	 */
+	private function register_presenters() {
+		// Dashboard Page Presenter.
+		$this->set(
+			'dashboard_presenter',
+			function () {
+				return new Dashboard_Page( $this->get( 'notifications_repo' ) );
+			}
+		);
+
+		// Hooks Page Presenter.
+		$this->set(
+			'hooks_presenter',
+			function () {
+				return new Hooks_Page( $this->get( 'custom_hooks_repo' ) );
+			}
+		);
+
+		// Settings Page Presenter.
+		$this->set(
+			'settings_presenter',
+			function () {
+				return new Settings_Page();
+			}
+		);
 	}
 
 	/**
